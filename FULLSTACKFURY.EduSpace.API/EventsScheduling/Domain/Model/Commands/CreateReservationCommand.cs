2d2db85c7158da71b5d41d5ca20695aef6f3c99e
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-22 17:25:05 -0500

    feat(events-scheduling): commands added to command in the model package

--- FULLSTACKFURY.EduSpace.API/EventsScheduling/Domain/Model/Commands/CreateReservationCommand.cs
+++ FULLSTACKFURY.EduSpace.API/EventsScheduling/Domain/Model/Commands/CreateReservationCommand.cs
@@ -1,3 +1,3 @@
 namespace FULLSTACKFURY.EduSpace.API.EventsScheduling.Domain.Model.Commands;
 
-public record CreateReservationCommand();
+public record CreateReservationCommand(string Title, DateTime Start, DateTime End, int AreaId, int TeacherId);
