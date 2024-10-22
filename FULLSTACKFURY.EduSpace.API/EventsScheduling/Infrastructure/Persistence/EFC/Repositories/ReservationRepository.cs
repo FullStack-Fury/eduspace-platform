6d115e20be9b0d127be7867aba5177f0ca25a58f
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-22 16:50:55 -0500

    feat(events-scheduling): Reservation repository added to infrastructure layer

---
+++ FULLSTACKFURY.EduSpace.API/EventsScheduling/Infrastructure/Persistence/EFC/Repositories/ReservationRepository.cs
@@ -0,0 +1,17 @@
+using FULLSTACKFURY.EduSpace.API.EventsScheduling.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.EventsScheduling.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FULLSTACKFURY.EduSpace.API.EventsScheduling.Infrastructure.Persistence.EFC.Repositories;
+
+public class ReservationRepository(AppDbContext context) 
+    : BaseRepository<Reservation>(context), IReservationRepository
+    
+{
+    public async Task<IEnumerable<Reservation>> FindByAreaIdAsync(int areaId)
+    {
+        return await Context.Set<Reservation>().Where(f => f.AreaId.Id == areaId).ToListAsync();
+    }
+}
