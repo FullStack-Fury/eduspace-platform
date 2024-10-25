8e6d50aee0ee057983f33568c21c9e7ac55168d8
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-25 13:17:12 -0500

    feat(payroll-management): value objects created and setted.

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/ValueObjects/SalaryAmount.cs
@@ -0,0 +1,15 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.ValueObjects
+{
+    public record SalaryAmount
+    {
+        public decimal Value { get; }
+
+        public SalaryAmount(decimal value)
+        {
+            if (value < 0) throw new ArgumentException("Salary amount cannot be negative.");
+            Value = value;
+        }
+
+        public static implicit operator decimal(SalaryAmount amount) => amount.Value;
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/ValueObjects/SalaryBonus.cs
@@ -0,0 +1,15 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.ValueObjects
+{
+    public record SalaryBonus
+    {
+        public decimal Value { get; }
+
+        public SalaryBonus(decimal value)
+        {
+            if (value < 0) throw new ArgumentException("Salary bonus cannot be negative.");
+            Value = value;
+        }
+
+        public static implicit operator decimal(SalaryBonus bonus) => bonus.Value;
+    }
+}
