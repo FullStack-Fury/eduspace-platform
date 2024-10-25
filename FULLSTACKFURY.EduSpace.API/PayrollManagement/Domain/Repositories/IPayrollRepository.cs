38187eee0c9d0bce14f1c7a92da796eaaca6ccb1
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-25 13:22:46 -0500

    feat(payroll-management): payroll interface repository added to domain layer.

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Repositories/IPayrollRepository.cs
@@ -0,0 +1,14 @@
+﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates;
+
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Repositories
+{
+    public interface IPayrollRepository
+    {
+        Task<IEnumerable<Payroll>> GetAllAsync();
+        Task<Payroll?> GetByIdAsync(int id);
+        Task AddAsync(Payroll payroll);
+        void Update(Payroll payroll);
+        void Delete(Payroll payroll);
+        Task<IEnumerable<Payroll>> FindByTeacherIdAsync(int teacherId);
+    }
+}
