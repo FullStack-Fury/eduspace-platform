1be766df513a1931c1df4b0b788cff24568b67a4
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-25 13:20:25 -0500

    feat(payroll-management): payroll command and query interface added to domain layer.

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/services/IPayrollCommandService.cs
@@ -0,0 +1,11 @@
+﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Commands;
+
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Services
+{
+    public interface IPayrollCommandService
+    {
+        Task<Payroll?> Handle(CreatePayrollCommand command);
+        Task<Payroll?> Handle(UpdatePayrollCommand command);
+    }
+}   
---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/services/IPayrollQueryService.cs
@@ -0,0 +1,11 @@
+﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Queries;
+
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Services
+{
+    public interface IPayrollQueryService
+    {
+        Task<IEnumerable<Payroll>> Handle(GetAllPayrollsQuery query);
+        Task<Payroll?> Handle(GetPayrollByTeacherIdQuery query);
+    }
+}
