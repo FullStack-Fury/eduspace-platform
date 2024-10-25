e1bb94a78a2ead65941b630ee88325ee97c38c6d
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-25 13:35:05 -0500

    chore: updated program.cs

--- FULLSTACKFURY.EduSpace.API/Program.cs
+++ FULLSTACKFURY.EduSpace.API/Program.cs
@@ -1,13 +1,49 @@
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configurar la cadena de conexión a la base de datos
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' is not set.");
+}
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging(builder.Environment.IsDevelopment())
+        .EnableDetailedErrors();
+});
+
 var app = builder.Build();
+
+// Verificar y crear la base de datos si no existe
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+
+    try
+    {
+        // Verificar y crear tablas si no existen
+        context.Database.EnsureCreated();
+    }
+    catch (Exception ex)
+    {
+        // Aquí puedes manejar la excepción o registrar el error
+        Console.WriteLine($"Error al crear la base de datos: {ex.Message}");
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,9 +53,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
