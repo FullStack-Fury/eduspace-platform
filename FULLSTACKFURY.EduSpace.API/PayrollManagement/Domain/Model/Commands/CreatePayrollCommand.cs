a421b299c68d9a53005e38ccbb869995c99f3acd
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-25 13:07:39 -0500

    feat(payroll-management): commands added

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/Commands/CreatePayrollCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Commands
+{
+    public class CreatePayrollCommand
+    {
+        public int TeacherId { get; }
+        public decimal SalaryAmount { get; }
+        public decimal PensionContribution { get; }
+        public decimal SalaryBonus { get; }
+        public decimal OtherDeductions { get; }
+
+        public CreatePayrollCommand(int teacherId, decimal salaryAmount, decimal pensionContribution, decimal salaryBonus, decimal otherDeductions)
+        {
+            TeacherId = teacherId;
+            SalaryAmount = salaryAmount;
+            PensionContribution = pensionContribution;
+            SalaryBonus = salaryBonus;
+            OtherDeductions = otherDeductions;
+        }
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/Commands/UpdatePayrollCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Commands
+{
+    public class UpdatePayrollCommand
+    {
+        public int Id { get; }
+        public decimal SalaryAmount { get; }
+        public decimal PensionContribution { get; }
+        public decimal SalaryBonus { get; }
+        public decimal OtherDeductions { get; }
+
+        public UpdatePayrollCommand(int id, decimal salaryAmount, decimal pensionContribution, decimal salaryBonus, decimal otherDeductions)
+        {
+            Id = id;
+            SalaryAmount = salaryAmount;
+            PensionContribution = pensionContribution;
+            SalaryBonus = salaryBonus;
+            OtherDeductions = otherDeductions;
+        }
+    }
+}
