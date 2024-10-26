7b0c0fad9b4a46a0354166a80c3dd328806892af
AngeloC12 <angelocuri@hotmail.com>
2024-10-26 02:21:42 -0500

    feat: created GetReportByResourceIdQuery

---
+++ FULLSTACKFURY.EduSpace.API/BreakdownManagement/Domain/Model/Queries/GetReportByResourceIdQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.BreakdownManagement.Domain.Model.Queries
+{
+    public class GetReportByResourceIdQuery
+    {
+        public int ResourceId { get; }
+
+        public GetReportByResourceIdQuery(int resourceId)
+        {
+            ResourceId = resourceId;
+        }
+    }
+}
