aab037a3962134eab703161a2df8382c56760a51
AngeloC12 <angelocuri@hotmail.com>
2024-10-26 02:22:52 -0500

    feat: created Repository, Report.cs

---
+++ FULLSTACKFURY.EduSpace.API/BreakdownManagement/Domain/Model/Aggregates/Report.cs
@@ -0,0 +1,31 @@
+﻿using FULLSTACKFURY.EduSpace.API.BreakdownManagement.Domain.Model.ValueObjects;
+
+namespace FULLSTACKFURY.EduSpace.API.BreakdownManagement.Domain.Model.Aggregates
+{
+    public class Report
+    {
+        public int Id { get; private set; }
+        public string KindOfReport { get; private set; }
+        public string Description { get; private set; }
+        public ResourceId ResourceId { get; private set; }
+        public ReportDate CreatedAt { get; private set; }
+
+        // Constructor sin parámetros para EF Core
+        private Report() { }
+
+        public Report(string kindOfReport, string description, ResourceId resourceId, ReportDate createdAt)
+        {
+            KindOfReport = kindOfReport;
+            Description = description;
+            ResourceId = resourceId;
+            CreatedAt = createdAt;
+        }
+
+        public void UpdateReport(string kindOfReport, string description, ResourceId resourceId)
+        {
+            KindOfReport = kindOfReport;
+            Description = description;
+            ResourceId = resourceId;
+        }
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/BreakdownManagement/Domain/Repositories/IReportRepository.cs
@@ -0,0 +1,14 @@
+﻿using FULLSTACKFURY.EduSpace.API.BreakdownManagement.Domain.Model.Aggregates;
+
+namespace FULLSTACKFURY.EduSpace.API.BreakdownManagement.Domain.Repositories
+{
+    public interface IReportRepository
+    {
+        Task<IEnumerable<Report>> GetAllAsync();
+        Task<Report?> GetByIdAsync(int id);
+        Task AddAsync(Report report);
+        void Update(Report report);
+        void Delete(Report report);
+        Task<IEnumerable<Report>> FindByResourceIdAsync(int resourceId);
+    }
+}
