4b1efe1002a4258fc1f10256a2b58aa6285240f4
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-27 17:16:53 -0500

    feat(spaces-and-resource-management): added IClassroomRepository in the Domain

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Repositories/IClassroomRepository.cs
@@ -0,0 +1,32 @@
+﻿using FULLSTACKFURY.EduSpace.API.Shared.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+
+/// <summary>
+/// Represents the Classroom repository in the Platform. 
+/// </summary>
+public interface IClassroomRepository : IBaseRepository<Classroom> 
+{
+    /// <summary>
+    /// Finds a Classroom by classroom id asynchronously. 
+    /// </summary>
+    /// <param name="classroomId">
+    /// The id of the classroom to find classrooms by.
+    /// </param>
+    /// <returns>
+    /// A collection of classroom that belong to the resource.
+    /// </returns>
+    Task<IEnumerable<Classroom>> FindByIdAsync(int classroomId);
+    
+    /// <summary>
+    /// Finds a Classroom by teacher id asynchronously.
+    /// </summary>
+    /// <param name="teacherId">
+    /// The id of the teacher to find classrooms by.
+    /// </param>
+    /// <returns>
+    /// A collection of classroom that belong to the teacher.
+    /// </returns>
+    Task<IEnumerable<Classroom>> FindByTeacherIdAsync(int teacherId);
+}
