520fa94dd7c459e331ba756e5521c1a79e886c39
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-27 17:15:16 -0500

    feat(spaces-and-resource-management): added IResourceQueryService

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Services/IResourceQueryService.cs
@@ -0,0 +1,10 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_mangement.Domain.Model.Queries;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+public interface IResourceQueryService
+{
+    Task<IEnumerable<Resource>> Handle(GetAllResourcesQuery query); 
+    Task<Resource?> Handle(GetResourceByIdQuery query);
+}
