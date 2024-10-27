3f0b30101403098228e274aa9d77e627c1e33e06
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-27 17:18:23 -0500

    feat(spaces-and-resource-management): added CreateResourceCommand

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Commands/CreateResourceCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+
+/// <summary>
+/// Command to create a resource. 
+/// </summary>
+/// <param name="Name">
+/// The name of the resource to create.
+/// </param>
+/// <param name="Kind_of_resource">
+/// The kind of resource of the resource  to create.
+/// </param>
+/// <param name="ClassroomId">
+/// The ID of the Classroom for the resource.
+/// </param>
+public record CreateResourceCommand(string Name, string Kind_of_resource, int ClassroomId);
