672b2a76f40e49f6c4823f46db2842a3ca2deeb8
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-27 17:16:21 -0500

    feat(spaces-and-resource-management): added IClassroomClassroomService

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Services/IClassroomCommandService.cs
@@ -0,0 +1,21 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+/// <summary>
+/// Represents the Classroom command service in the Platform. 
+/// </summary>
+public interface IClassroomCommandService
+{
+    /// <summary>
+    /// Handles the create Classroom command in the Platform. 
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="CreateClassroomCommand"/> command to handle.
+    /// </param>
+    /// <returns>
+    /// The created <see cref="Resource"/> entity.
+    /// </returns>
+    public Task<Classroom?> Handle(CreateClassroomCommand command);
+}
