a69d746bc5aececcba54871c7cc82ce573eb1e8f
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-27 17:15:38 -0500

    feat(spaces-and-resource-management): added IResourceCommandService

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Services/IResourceCommandService.cs
@@ -0,0 +1,9 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+public interface IResourceCommandService
+{
+    public Task<Resource?> Handle(CreateResourceCommand command);
+}
