b94376e7410574b3c566c8828cfa040619cd960e
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-27 17:18:53 -0500

    feat(spaces-and-resource-management): create the ClassroomCommandService in the application

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Application/Internal/CommandServices/ClassroomCommandService.cs
@@ -0,0 +1,19 @@
+﻿using FULLSTACKFURY.EduSpace.API.Shared.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Application.Internal.CommandServices;
+
+public class ClassroomCommandService(IClassroomRepository classroomRepository, IUnitOfWork unitOfWork) : IClassroomCommandService
+{
+    public async Task<Classroom?> Handle(CreateClassroomCommand command)
+    {
+        var classroom = new Classroom(command);
+
+        await classroomRepository.AddAsync(classroom);
+        await unitOfWork.CompleteAsync();
+        return classroom;
+    }
+}
