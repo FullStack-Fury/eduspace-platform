28cbf67fd0f702781e599cca493fc528f32cc91b
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-28 05:37:38 -0500

    commit(payroll): added

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Application/Internal/QueryServices/TeacherQueryService.cs
@@ -0,0 +1,23 @@
+﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Application.Internal.QueryServices
+{
+    public class TeacherQueryService
+    {
+        private readonly AppDbContext _context;
+
+        public TeacherQueryService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Teacher>> GetAllTeachers()
+        {
+            return await _context.Teachers.ToListAsync();
+        }
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/Aggregates/Teacher.cs
@@ -0,0 +1,14 @@
+﻿// Teacher.cs
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Teacher(string name)
+        {
+            Name = name;
+        }
+    }
+}
