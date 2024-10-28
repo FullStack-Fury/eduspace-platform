c353ad40befdea4b73cdfb2ebed9d0f9f0aaadc1
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-28 05:36:26 -0500

    feat(payroll): update payroll.cs

--- FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/Aggregates/Payroll.cs
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/Aggregates/Payroll.cs
@@ -1,4 +1,5 @@
 ﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.ValueObjects;
+using System;
 
 namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates
 {
@@ -6,32 +7,54 @@ namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates
     {
         public int Id { get; private set; }
         public int TeacherId { get; private set; }
-        public SalaryAmount? SalaryAmount { get; private set; }  // Anulable
-        public PensionContribution? PensionContribution { get; private set; }  // Anulable
-        public SalaryBonus? SalaryBonus { get; private set; }  // Anulable
-        public OtherDeductions? OtherDeductions { get; private set; }  // Anulable
-    
-        public decimal SalaryNet => SalaryAmount?.Value ?? 0 + SalaryBonus?.Value ?? 0 - PensionContribution?.Value ?? 0 - OtherDeductions?.Value ?? 0;
+        public SalaryAmount? SalaryAmount { get; private set; }
+        public PensionContribution? PensionContribution { get; private set; }
+        public SalaryBonus? SalaryBonus { get; private set; }
+        public OtherDeductions? OtherDeductions { get; private set; }
+        public SalaryNet SalaryNet { get; private set; } // Usamos el ValueObject para SalaryNet
 
-        // Constructor sin parámetros para EF Core
+        public Teacher Teacher { get; private set; }
+
+        public DatePayment? DatePayment { get; private set; }
+        public PaymentMethod? PaymentMethod { get; private set; }
+        public Account? Account { get; private set; }
+        public Observation? Observation { get; private set; }
+
         private Payroll() { }
 
-        public Payroll(int teacherId, SalaryAmount salaryAmount, PensionContribution pensionContribution, SalaryBonus salaryBonus, OtherDeductions otherDeductions)
+        public Payroll(int teacherId, SalaryAmount salaryAmount, PensionContribution pensionContribution, SalaryBonus salaryBonus, OtherDeductions otherDeductions,
+                       DatePayment datePayment, PaymentMethod paymentMethod, Account account, Observation observation)
         {
             TeacherId = teacherId;
             SalaryAmount = salaryAmount;
             PensionContribution = pensionContribution;
             SalaryBonus = salaryBonus;
             OtherDeductions = otherDeductions;
+            DatePayment = datePayment;
+            PaymentMethod = paymentMethod;
+            Account = account;
+            Observation = observation;
+            CalculateSalaryNet();
         }
 
-        public void UpdatePayroll(SalaryAmount salaryAmount, PensionContribution pensionContribution, SalaryBonus salaryBonus, OtherDeductions otherDeductions)
+        public void UpdatePayroll(SalaryAmount salaryAmount, PensionContribution pensionContribution, SalaryBonus salaryBonus, OtherDeductions otherDeductions,
+                                  DatePayment datePayment, PaymentMethod paymentMethod, Account account, Observation observation)
         {
             SalaryAmount = salaryAmount;
             PensionContribution = pensionContribution;
             SalaryBonus = salaryBonus;
             OtherDeductions = otherDeductions;
+            DatePayment = datePayment;
+            PaymentMethod = paymentMethod;
+            Account = account;
+            Observation = observation;
+            CalculateSalaryNet();
+        }
+
+        private void CalculateSalaryNet()
+        {
+            var netValue = (SalaryAmount?.Value ?? 0) + (SalaryBonus?.Value ?? 0) - (PensionContribution?.Value ?? 0) - (OtherDeductions?.Value ?? 0);
+            SalaryNet = new SalaryNet(netValue);
         }
     }
-
 }
