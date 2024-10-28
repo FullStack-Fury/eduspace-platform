1439d830c7c300587a2ef3989fbdf196fc7b6674
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-28 07:48:24 -0500

    feat(payroll-query): update payroll query.

--- FULLSTACKFURY.EduSpace.API/PayrollManagement/Application/Internal/QueryServices/PayrollQueryService.cs
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Application/Internal/QueryServices/PayrollQueryService.cs
@@ -2,26 +2,38 @@
 using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Repositories;
 using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Queries;
 using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Application.Internal.QueryServices
 {
     public class PayrollQueryService : IPayrollQueryService
     {
         private readonly IPayrollRepository _payrollRepository;
+        private readonly AppDbContext _context;
 
-        public PayrollQueryService(IPayrollRepository payrollRepository)
+        public PayrollQueryService(IPayrollRepository payrollRepository, AppDbContext context)
         {
             _payrollRepository = payrollRepository;
+            _context = context;
         }
 
         public async Task<IEnumerable<Payroll>> Handle(GetAllPayrollsQuery query)
         {
-            return await _payrollRepository.GetAllAsync();
+            // Usar Include para cargar la entidad Teacher relacionada con Payroll
+            return await _context.Payrolls
+                .Include(p => p.Teacher) // Cargar la entidad Teacher
+                .ToListAsync();
         }
 
         public async Task<Payroll?> Handle(GetPayrollByTeacherIdQuery query)
         {
-            return (await _payrollRepository.FindByTeacherIdAsync(query.TeacherId)).FirstOrDefault();
+            return await _context.Payrolls
+                .Include(p => p.Teacher) // Cargar la entidad Teacher
+                .FirstOrDefaultAsync(p => p.TeacherId == query.TeacherId);
         }
     }
 }
