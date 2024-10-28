ad20ead673650f05eb63fe1a325f07c351062d12
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-28 05:36:04 -0500

    feat(value-objects): created more value objects

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/ValueObjects/Observation.cs
@@ -0,0 +1,18 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.ValueObjects
+{
+    public record Observation
+    {
+        public string Value { get; }
+
+        public Observation(string value)
+        {
+            // La observación puede ser vacía o nula, ya que podría no haber observaciones.
+            Value = value;
+        }
+
+        // Constructor sin parámetros para EF Core
+        private Observation() { }
+
+        public static implicit operator string(Observation observation) => observation.Value;
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Domain/Model/ValueObjects/PaymentMethod.cs
@@ -0,0 +1,18 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.ValueObjects
+{
+    public record PaymentMethod
+    {
+        public string Value { get; }
+
+        public PaymentMethod(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("El método de pago no puede estar vacío.");
+            Value = value;
+        }
+
+        // Constructor sin parámetros para EF Core
+        private PaymentMethod() { }
+
+        public static implicit operator string(PaymentMethod paymentMethod) => paymentMethod.Value;
+    }
+}
