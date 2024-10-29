b25a458481b927428d6d9ecd53e774d0fd1263fe
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-29 18:43:30 -0500

    feat(iam): resources added to iam interface

---
+++ FULLSTACKFURY.EduSpace.API/IAM/Interface/Resources/AccountResource.cs
@@ -0,0 +1,3 @@
+namespace FULLSTACKFURY.EduSpace.API.IAM.Interface.Resources;
+
+public record AccountResource(int Id, string Username);
---
+++ FULLSTACKFURY.EduSpace.API/IAM/Interface/Resources/SignInResource.cs
@@ -0,0 +1,3 @@
+namespace FULLSTACKFURY.EduSpace.API.IAM.Interface.Resources;
+
+public record SignInResource(string Username, string Password);
