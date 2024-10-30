1035bf8298ec97c1dd1061f6ff485b03b29a54c8
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 18:22:58 -0500

    feat: create the classroom and teacher resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Resources/ClassroomResource.cs
@@ -0,0 +1,18 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+/// <summary>
+/// Represents the data exposed by the tutorial resource. 
+/// </summary>
+/// <param name="Id">
+/// The unique identifier of the tutorial.
+/// </param>
+/// <param name="Name">
+/// The name of the classroom.
+/// </param>
+/// <param name="Description">
+/// The description of the classroom.
+/// </param>
+/// <param name="Teacher">
+/// The <see cref="TeacherResource"/> teacher of the classroom.
+/// </param>
+public record ClassroomResource(int Id, string Name, string Description, TeacherResource Teacher); 
