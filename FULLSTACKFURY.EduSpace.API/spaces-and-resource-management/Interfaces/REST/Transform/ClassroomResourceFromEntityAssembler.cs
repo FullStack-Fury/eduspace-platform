0b11510d01cf5f84d4fdee47dd58088fdc11cea2
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 18:45:39 -0500

    feat: create the transform for classroom and teacher resource from entity assembler

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/ClassroomResourceFromEntityAssembler.cs
@@ -0,0 +1,25 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to transform Classroom to ClassroomResource
+/// </summary>
+public class ClassroomResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Transform Classroom to ClassroomResource
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="Classroom"/> entity to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="ClassroomResource"/> resource with the values from the entity
+    /// </returns>
+    public static ClassroomResource ToResourceFromEntity(Classroom entity)
+    {
+        return new ClassroomResource(entity.Id, entity.Name, entity.Description,
+            TeacherResourceFromEntityAssembler.ToResourceFromEntity(entity.Teacher));
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/TeacherResourceFromEntityAssembler.cs
@@ -0,0 +1,21 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Entities;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+
+public static class TeacherResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Transform Category to CategoryResource 
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="Teacher"/> entity to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="TeacherResource"/> resource with the values from the entity
+    /// </returns>
+    public static TeacherResource ToResourceFromEntity(Teacher entity)
+    {
+        return new TeacherResource(entity.Id, entity.Name, entity.Email, entity.Phone, entity.Location);
+    }
+}
