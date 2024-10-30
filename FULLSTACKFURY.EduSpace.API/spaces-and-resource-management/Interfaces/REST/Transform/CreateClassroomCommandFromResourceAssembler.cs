dc0b26779b45fc763f81cf7a821186c04d04a83b
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 18:46:12 -0500

    feat: create the command from resource assembler for create classroom and teacher

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/CreateClassroomCommandFromResourceAssembler.cs
@@ -0,0 +1,24 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to transform CreateClassroomResource to CreateClassroomCommand
+/// </summary>
+public class CreateClassroomCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Transform CreateClassroomResource to CreateClassroomCommand
+    /// </summary>
+    /// <param name="resource">
+    /// The <see cref="CreateClassroomResource"/> resource to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="CreateClassroomCommand"/> command with the values from the resource
+    /// </returns>
+    public static CreateClassroomCommand ToCommandFromResource(CreateClassroomResource resource)
+    {
+        return new CreateClassroomCommand(resource.Name, resource.Description, resource.TeacherId);
+    }
+}
