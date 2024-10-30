453201a63770d6fbf3eb978f6cbbfd82c4562a9b
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 18:25:36 -0500

    feat: create the record to create the classroom and teacher resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Resources/CreateTeacherResource.cs
@@ -0,0 +1,18 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+/// <summary>
+///  Represent a resource to create a teacher.
+/// </summary>
+/// <param name="Name">
+/// The name of the Teacher
+/// </param>
+/// <param name="Email">
+/// The email of the Teacher
+/// </param>
+/// <param name="Phone">
+/// The phone number of the Teacher
+/// </param>
+/// <param name="Location">
+/// The localization of the Teacher
+/// </param>
+public record CreateTeacherResource(string Name, string Email, string Phone, string Location);
