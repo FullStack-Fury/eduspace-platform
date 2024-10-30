612b0f20194a3a2ce4dfd7b33705b5623a59826a
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 17:19:33 -0500

    feat: create the Query service to Classroom and teacher

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Application/Internal/QueryServices/ClassroomQueryService.cs
@@ -0,0 +1,33 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Application.Internal.QueryServices;
+
+/// <summary>
+/// Classroom query service
+/// </summary>
+/// <param name="classroomRepository">
+/// The classroom repository
+/// </param>
+public class ClassroomQueryService(IClassroomRepository classroomRepository): IClassroomQueryService
+{
+    /// <inheritdoc />
+    public async Task<Classroom?> Handle(GetClassroomByIdQuery query)
+    {
+        return await classroomRepository.FindByIdAsync(query.ClassroomId);
+    }
+    
+    /// <inheritdoc />
+    public async Task<IEnumerable<Classroom>> Handle(GetAllClassroomsQuery query)
+    {
+        return await classroomRepository.ListAsync();
+    }
+    
+    /// <inheritdoc />
+    public async Task<IEnumerable<Classroom>> Handle(GetAllClassroomsByTeacherIdQuery query)
+    {
+        return await classroomRepository.FindByTeacherIdAsync(query.TeacherId);
+    }
+}
