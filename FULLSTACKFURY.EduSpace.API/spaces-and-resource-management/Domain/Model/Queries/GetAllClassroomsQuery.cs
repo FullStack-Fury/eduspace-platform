9bdfb21bcfedc3fbe7b3dea1106a0dfdf899d764
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 16:55:24 -0500

    feat: create all queries to get classrooms and teachers

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Queries/GetAllClassroomsQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+
+/// <summary>
+/// Represents a query to get all classrooms in the EduSpace.
+/// </summary>
+public record GetAllClassroomsQuery;
--- FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Queries/GetClassroomByIdQuery.cs
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Queries/GetClassroomByIdQuery.cs
@@ -1,9 +1,9 @@
-﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_mangement.Domain.Model.Queries;
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
 
 /// <summary>
-/// Represents a query to get a classroom by id in the Platform. 
+/// Represents a query to get a classroom by id in the EduSpace API. 
 /// </summary>
 /// <param name="ClassroomId">
-/// The id of the Classroom to get
+/// The id of the classroom to get
 /// </param>
 public record GetClassroomByIdQuery(int ClassroomId);
