8a387bf08e11557c452d6a4356983a072cc8f1c2
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 11:04:18 -0500

    feat: create the create resource resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Resources/CreateResourceResource.cs
@@ -0,0 +1,15 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+/// <summary>
+/// The resource to create a new resource
+/// </summary>
+/// <param name="Name">
+/// The name of the resource
+/// </param>
+/// <param name="KindOfResource">
+/// The kind of resource
+/// </param>
+/// <param name="ClassroomId">
+/// The id of the Classroom
+/// </param>
+public record CreateResourceResource(string Name, string KindOfResource, int ClassroomId);
