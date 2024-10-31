4703648c178a2de306f5d7fae7dcd067f9943b20
Oscar Antayhua <oscarjac2001@gmail.com>
2024-10-30 21:21:08 -0500

    feat(payroll): set interface transform

---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Interface/REST/Transform/CreatePayrollCommandFromResourceAssembler.cs
@@ -0,0 +1,33 @@
+﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.PayrollManagement.Interface.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Interface.REST.Transform;
+
+/// <summary>
+/// Assembler class to transform CreatePayrollResource to CreatePayrollCommand
+/// </summary>
+public static class CreatePayrollCommandFromResourceAssembler
+{
+    /// <summary>
+    /// Transform CreatePayrollResource to CreatePayrollCommand
+    /// </summary>
+    /// <param name="resource">
+    /// The <see cref="CreatePayrollResource"/> resource to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="CreatePayrollCommand"/> command with the values from the resource
+    /// </returns>
+    public static CreatePayrollCommand ToCommand(CreatePayrollResource resource)
+    {
+        return new CreatePayrollCommand(
+            resource.TeacherId,
+            resource.SalaryAmount,
+            resource.PensionContribution,
+            resource.SalaryBonus,
+            resource.OtherDeductions,
+            resource.DatePayment,
+            resource.PaymentMethod,
+            resource.Account,
+            resource.Observation);
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/PayrollManagement/Interface/REST/Transform/PayrollResourceFromEntityAssembler.cs
@@ -0,0 +1,34 @@
+﻿using FULLSTACKFURY.EduSpace.API.PayrollManagement.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.PayrollManagement.Interface.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.PayrollManagement.Interface.REST.Transform;
+
+/// <summary>
+/// Assembler class to transform Payroll to PayrollResource
+/// </summary>
+public static class PayrollResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Transform Payroll entity to PayrollResource
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="Payroll"/> entity to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="PayrollResource"/> with the values from the entity
+    /// </returns>
+    public static PayrollResource ToResourceFromEntity(Payroll entity)
+    {
+        return new PayrollResource(
+            entity.Id,
+            entity.TeacherId,
+            entity.SalaryAmount.Value,
+            entity.PensionContribution.Value,
+            entity.SalaryBonus.Value,
+            entity.OtherDeductions.Value,
+            entity.DatePayment.Value,
+            entity.PaymentMethod.Value,
+            entity.Observation?.Value,  // Sin Account
+            entity.SalaryNet.Value);
+    }
+}
