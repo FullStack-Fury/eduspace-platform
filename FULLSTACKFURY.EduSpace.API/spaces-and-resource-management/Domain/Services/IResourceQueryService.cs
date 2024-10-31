c128993db85c25bfc1f69c197406a1567908ced1
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 10:58:48 -0500

    feat: create the interface query service to resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Services/IResourceQueryService.cs
@@ -0,0 +1,40 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+public interface IResourceQueryService
+{
+    /// <summary>
+    /// Handles the get resource by id query in the EduSpace API.
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetResourceByIdQuery"/> query to handle.
+    /// </param>
+    /// <returns>
+    /// The <see cref="Resource"/> entity.
+    /// </returns>
+    Task<Resource?> Handle(GetResourceByIdQuery query);
+    
+    /// <summary>
+    /// Handles the get all resource query in the EduSpace API.
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetAllResourcesQuery"/> query to handle.
+    /// </param>
+    /// <returns>
+    /// A collection of all classrooms in the platform.
+    /// </returns>
+    Task<IEnumerable<Resource>> Handle(GetAllResourcesQuery query);
+    
+    /// <summary>
+    /// Handles the get all resources by classroom id query in the EduSpace API.
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetAllResourcesByClassroomIdQuery"/> query to handle.
+    /// </param>
+    /// <returns>
+    /// A collection of resource  that belong to the classroom.
+    /// </returns>
+    Task<IEnumerable<Resource>> Handle(GetAllResourcesByClassroomIdQuery query);
+}
