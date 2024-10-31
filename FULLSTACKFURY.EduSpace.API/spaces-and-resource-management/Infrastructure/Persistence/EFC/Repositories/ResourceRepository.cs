206c8f9a2c6c736022158921d48fb06cf8193932
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 11:02:33 -0500

    feat: create the repository to resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Infrastructure/Persistence/EFC/Repositories/ResourceRepository.cs
@@ -0,0 +1,42 @@
+﻿using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Infrastructure.Persistence.EFC.Repositories;
+
+public class ResourceRepository(AppDbContext context): BaseRepository<Resource>(context), IResourceRepository
+{
+    // inheritedDoc
+    public async Task<IEnumerable<Resource>> FindByClassroomIdAsync(int classroomId)
+    {
+        return await Context.Set<Resource>()
+            .Include(resource => resource.Classroom)
+            .Where(resource => resource.ClassroomId == classroomId)
+            .ToListAsync(); 
+    }
+    
+    // inheritedDoc
+    public new async Task<Resource?> FindByIdAsync(int id)
+    {
+        return await Context.Set<Resource>()
+            .Include(resource => resource.Classroom)
+            .FirstOrDefaultAsync(resource => resource.Id == id);
+    }
+    
+    // inheritedDoc
+    public async Task<IEnumerable<Resource>> ListAsync()
+    {
+        return await Context.Set<Resource>()
+            .Include(resource => resource.Classroom)
+            .ToListAsync();
+    }
+    
+    // inheritedDoc
+    public async Task<bool> ExistsByNameAsync(string name)
+    {
+        return await Context.Set<Resource>()
+            .AnyAsync(resource => resource.Name == name);
+    }
+}
