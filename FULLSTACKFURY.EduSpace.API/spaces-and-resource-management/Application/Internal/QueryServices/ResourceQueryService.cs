9f53f31191d7c01d6d839cb1ac8db3add2a8b5d0
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 11:01:42 -0500

    feat: Create the query service to resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Application/Internal/QueryServices/ResourceQueryService.cs
@@ -0,0 +1,33 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Application.Internal.QueryServices;
+
+/// <summary>
+/// The resource query service
+/// </summary>
+/// <param name="resourceRepository">
+/// The resource repository
+/// </param>
+public class ResourceQueryService(IResourceRepository resourceRepository): IResourceQueryService
+{
+    // <inheritdoc />
+    public async Task<Resource?> Handle(GetResourceByIdQuery query)
+    {
+        return await resourceRepository.FindByIdAsync(query.ResourceId);
+    }
+    
+    // <inheritdoc />
+    public async Task<IEnumerable<Resource>> Handle(GetAllResourcesQuery query)
+    {
+        return await resourceRepository.ListAsync();
+    }
+    
+    // <inheritdoc />
+    public async Task<IEnumerable<Resource>> Handle(GetAllResourcesByClassroomIdQuery query)
+    {
+        return await resourceRepository.FindByClassroomIdAsync(query.ClassroomId);
+    }
+}
