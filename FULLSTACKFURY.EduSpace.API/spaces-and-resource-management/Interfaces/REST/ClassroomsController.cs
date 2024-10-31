1fa8eeea267eaca75cf1de362f6e1a98bf8e4457
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-30 21:23:15 -0500

    feat: create the controller class for the endpoints

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/ClassroomsController.cs
@@ -0,0 +1,90 @@
+﻿using System.Net.Mime;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+
+[ApiController]
+[Route("api/vi/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Classroom Endpoints")]
+public class ClassroomsController(IClassroomQueryService classroomQueryService, IClassroomCommandService classroomCommandService) : ControllerBase
+{
+    /// <summary>
+    /// Get classroom by id
+    /// </summary>
+    /// <param name="classroomId">
+    /// The classroom id to get
+    /// </param>
+    /// <returns>
+    /// The <see cref="ClassroomResource"/> classroom if found, otherwise returns <see cref="NotFoundResult"/>
+    /// </returns>
+    [HttpGet("{classroomId:int}")]
+    [SwaggerOperation(
+        Summary = "Get a classroom by id",
+        Description = "Get a classroom by id",
+        OperationId = "GetClassroomById"
+    )]
+    [SwaggerResponse(StatusCodes.Status200OK, "The classroom was successfully retrieved", typeof(ClassroomResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "The classroom was not found")]
+    public async Task<IActionResult> GetClassroomById(int classroomId)
+    {
+        var getClassroomByIdQuery = new GetClassroomByIdQuery(classroomId);
+        var classroom = await classroomQueryService.Handle(getClassroomByIdQuery);
+        if (classroom is null) return NotFound();
+        var classroomResource = ClassroomResourceFromEntityAssembler.ToResourceFromEntity(classroom);
+        return Ok(classroomResource);
+    }
+    
+    /// <summary>
+    /// Create a classroom
+    /// </summary>
+    /// <param name="resource">
+    /// The <see cref="CreateClassroomResource"/> to create the classroom from
+    /// </param>
+    /// <returns>
+    /// The <see cref="ClassroomResource"/> classroom if created, otherwise returns <see cref="BadRequestResult"/>
+    /// </returns>
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create a classroom",
+        Description = "Create a classroom",
+        OperationId = "CreateClassroom"
+    )]
+    [SwaggerResponse(StatusCodes.Status201Created, "The classroom was successfully created", typeof(ClassroomResource))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "The classroom was not created")]
+    
+    public async Task<IActionResult> CreateClassroom([FromBody] CreateClassroomResource resource)
+    {
+        var createClassroomCommand = CreateClassroomCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var classroom = await classroomCommandService.Handle(createClassroomCommand);
+        if (classroom is null) return BadRequest();
+        var classroomResource = ClassroomResourceFromEntityAssembler.ToResourceFromEntity(classroom);
+        return CreatedAtAction(nameof(GetClassroomById), new {classroomId = classroom.Id}, classroomResource);
+    }
+
+    /// <summary>
+    /// Get all classrooms 
+    /// </summary>
+    /// <returns>
+    /// The list of <see cref="ClassroomResource"/> classrooms
+    /// </returns>
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get all classrooms",
+        Description = "Get all classrooms",
+        OperationId = "GetAllClassrooms"
+    )]
+    [SwaggerResponse(StatusCodes.Status200OK, "The classrooms were successfully retrieved",
+        typeof(IEnumerable<ClassroomResource>))]
+    public async Task<IActionResult> GetAllClassrooms()
+    {
+        var getAllClassroomsQuery = new GetAllClassroomsQuery();
+        var classrooms = await classroomQueryService.Handle(getAllClassroomsQuery);
+        var classroomResources = classrooms.Select(ClassroomResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(classroomResources);        
+    }    
+}
---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/TeachersController.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Mime;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")] // Route is the path to the controller
+[Produces(MediaTypeNames.Application.Json)] // Produces is the content type that the controller will return
+[SwaggerTag("Available Teachers Endpoints")] 
+public class TeachersController(ITeacherCommandService teacherCommandService, ITeacherQueryService teacherQueryService) : ControllerBase
+{
+    [HttpGet("{teacherId:int}")]
+    [SwaggerOperation(
+        Summary = "Get Teacher by Id",
+        Description = "Get a teacher by its unique identifier",
+        OperationId = "GetTeacherById")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The teacher was found", typeof(TeacherResource))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "The teacher was not found")]
+    public async Task<IActionResult> GetTeacherById(int teacherId)
+    {
+        var getTeacherByIdQuery = new GetTeacherByIdQuery(teacherId);
+        var teacher = await teacherQueryService.Handle(getTeacherByIdQuery);
+        if(teacher is null) return NotFound();
+        var teacherResource = TeacherResourceFromEntityAssembler.ToResourceFromEntity(teacher);
+        return Ok(teacherResource);        
+    }
+    
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create Teacher",
+        Description = "Create a new teacher",
+        OperationId = "CreateTeacher")]
+    [SwaggerResponse(StatusCodes.Status201Created, "The teacher was created", typeof(TeacherResource))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "The teacher was not created")]
+    public async Task<IActionResult> CreateTeacher([FromBody] CreateTeacherResource resource)
+    {
+        var createTeacherCommand = CreateTeacherCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var teacher = await teacherCommandService.Handle(createTeacherCommand);
+        if(teacher is null) return BadRequest();
+        var teacherResource = TeacherResourceFromEntityAssembler.ToResourceFromEntity(teacher);
+        return CreatedAtAction(nameof(GetTeacherById), new { teacherId = teacher.Id }, teacherResource);
+    }
+
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Get All Teachers",
+        Description = "Get all teachers",
+        OperationId = "GetAllTeachers")]
+    [SwaggerResponse(StatusCodes.Status200OK, "The teachers were found", typeof(IEnumerable<TeacherResource>))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "The teachers were not found")]
+    public async Task<IActionResult> GetAllTeachers()
+    {
+        var teachers = await teacherQueryService.Handle(new GetAllTeachersQuery());
+        var teacherResources = teachers.Select(TeacherResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(teacherResources);
+    }
+}
