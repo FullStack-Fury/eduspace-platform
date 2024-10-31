76693da6960a057a493a945bc548917e34e58c64
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 11:03:44 -0500

    feat: create te Resource to resource from entity assembler

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/ResourceResourceFromEntityAssembler.cs
@@ -0,0 +1,25 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler for converting Resource entities to Resource resources.
+/// </summary>
+public class ResourceResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Converts a <see cref="Resource"/> entity to a <see cref="ResourceResource"/> resource.
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="Resource"/> entity to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="ResourceResource"/> resource with the values from the entity
+    /// </returns>
+    public static ResourceResource ToResourceFromEntity(Resource entity)
+    {
+        return new ResourceResource(entity.Id, entity.Name, entity.KindOfResource,
+            ClassroomResourceFromEntityAssembler.ToResourceFromEntity(entity.Classroom));
+    }
+}
