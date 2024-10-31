f75ca92b14e4456f17a35135841b659e848b38e6
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 11:03:16 -0500

    feat: create to transform the resource to resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/CreateResourceCommandFromResourceAssembler.cs
@@ -0,0 +1,15 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler class to transform CreateClassroomResource to CreateClassroomCommand
+/// </summary>
+public class CreateResourceCommandFromResourceAssembler
+{
+    public static CreateResourceCommand ToCommandFromResource(CreateResourceResource resource)
+    {
+        return new CreateResourceCommand(resource.Name, resource.KindOfResource, resource.ClassroomId);
+    }
+}
