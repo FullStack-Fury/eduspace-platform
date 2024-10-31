08dce50346c5d9918e34e6c9f68cb1597edb0e7d
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 10:59:06 -0500

    feat: create the interface command to resource

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Services/IResourceCommandService.cs
@@ -0,0 +1,12 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+/// <summary>
+/// Service for handling commands related to resources.
+/// </summary>
+public interface IResourceCommandService
+{
+    Task<Resource?> Handle(CreateResourceCommand command);
+}
