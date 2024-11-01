f99011427b07f47b60e75c40ab154c7a859215bb
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-11-01 00:29:58 -0500

    feat: create the resource and Create resource to shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Resources/SharedArea/SharedAreaResource.cs
@@ -0,0 +1,15 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources.SharedArea;
+
+/// <summary>
+/// Represents the data required to create a new shared area.
+///  </summary>
+///  <param name="Name">
+///  The name of the shared area
+///  </param>
+///  <param name="Capacity">
+///  The capacity of the shared area
+///  </param>
+///  <param name="Description">
+///  The description of the shared area
+///  </param>
+public record SharedAreaResource(int Id, string Name, int Capacity, string Description);
