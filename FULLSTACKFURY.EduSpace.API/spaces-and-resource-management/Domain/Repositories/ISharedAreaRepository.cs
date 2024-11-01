f4d624d9fa33c3d690c705f5df0036ef5980767a
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 23:33:18 -0500

    feat: create the interface to repository to shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Repositories/ISharedAreaRepository.cs
@@ -0,0 +1,21 @@
+﻿using FULLSTACKFURY.EduSpace.API.Shared.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+
+/// <summary>
+///  Represents a repository for shared areas in the EduSpace API.
+/// </summary>
+public interface ISharedAreaRepository : IBaseRepository<sharedArea>
+{
+    /// <summary>
+    ///  Verify if a shared area with specified title exists.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the shared area to verify.
+    /// </param>
+    /// <returns>
+    /// True if the shared area exists, otherwise false.
+    /// </returns>
+    Task<bool> ExistsByNameAsync(string name);
+}
