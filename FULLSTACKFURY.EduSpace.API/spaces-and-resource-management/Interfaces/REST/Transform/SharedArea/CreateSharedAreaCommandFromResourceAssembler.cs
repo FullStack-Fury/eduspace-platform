849524435997fa726d35ddc714362d84f6fc81aa
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-11-01 00:37:27 -0500

    feat: create the assemblers to shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/SharedArea/CreateSharedAreaCommandFromResourceAssembler.cs
@@ -0,0 +1,24 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources.SharedArea;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform.SharedArea;
+
+/// <summary>
+///  Assembler class to transform CreateSharedAreaResource to CreateSharedAreaCommand
+///  </summary>
+public class CreateSharedAreaCommandFromResourceAssembler
+{
+    /// <summary>
+    ///  Transform CreateSharedAreaResource to CreateSharedAreaCommand
+    ///  </summary>
+    ///  <param name="resource">
+    ///  The <see cref="CreateSharedAreaResource"/> resource to transform
+    ///  </param>
+    ///  <returns>
+    ///  The resulting <see cref="CreateSharedAreaCommand"/> command with the values from the resource
+    ///  </returns>
+    public static CreateSharedAreaCommand ToCommandFromResource(CreateSharedAreaResource resource)
+    {
+        return new CreateSharedAreaCommand(resource.Name, resource.Capacity, resource.Description);
+    }
+}
---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Interfaces/REST/Transform/SharedArea/SharedAreaResourceFromEntityAssembler.cs
@@ -0,0 +1,23 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Resources.SharedArea;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Interfaces.REST.Transform.SharedArea;
+
+/// <summary>
+///  Assembler class to transform CreateSharedAreaResource to CreateSharedAreaCommand
+///  </summary>
+public class SharedAreaResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Transform shared area to shared area resource
+    /// </summary>
+    /// <param name="entity">
+    /// The <see cref="SharedArea"/> entity to transform
+    /// </param>
+    /// <returns>
+    /// The resulting <see cref="SharedAreaResource"/> resource with the values from the entity
+    /// </returns>
+    public static SharedAreaResource ToResourceFromEntity(Domain.Model.Aggregates.SharedArea entity)
+    {
+        return new SharedAreaResource(entity.Id, entity.Name, entity.Capacity, entity.Description);
+    }
+}
