6ba5e8db6d04416059402c0d8d8a4ca439800cde
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 23:13:16 -0500

    feat: create the aggregate sharedArea and SharedAreaAudit

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Aggregates/SharedAreaAudit.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+
+public class SharedAreaAudit
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Aggregates/sharedArea.cs
@@ -0,0 +1,47 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+
+/// <summary>
+/// Represents a shared area in the application.
+/// </summary>
+/// <remarks>
+/// This class is used to represent a shared area in the application.
+/// </remarks>
+public partial class sharedArea
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Capacity { get; set; }
+    public string Description { get; set; }
+    
+    /// <summary>
+    /// Default constructor for the shared area entity
+    /// </summary>
+    public sharedArea()
+    {
+    }
+    
+    /// <param name="name">
+    /// The name of the shared area
+    /// </param>
+    /// <param name="capacity">
+    /// The capacity of the shared area
+    /// </param>
+    /// <param name="description">
+    /// The description of the shared area
+    /// </param>
+    public sharedArea(string name, int capacity, string description): this()
+    {
+        Name = name;
+        Capacity = capacity;
+        Description = description;
+    }
+    
+    public sharedArea(CreateSharedAreaCommand command): this()
+    {
+        Name = command.Name;
+        Capacity = command.Capacity;
+        Description = command.Description;
+    }
+}
