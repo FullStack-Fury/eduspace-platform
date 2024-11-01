2ad8df2d6f4e5b4e81814cfac6f11d15fd6f6cb7
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-11-01 00:23:13 -0500

    feat: create the repository to shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Infrastructure/Persistence/EFC/Repositories/SharedAreaRepository.cs
@@ -0,0 +1,31 @@
+﻿using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using FULLSTACKFURY.EduSpace.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Infrastructure.Persistence.EFC.Repositories;
+
+public class SharedAreaRepository(AppDbContext context) : BaseRepository<SharedArea>(context), ISharedAreaRepository
+{
+    /// <Inheritdoc/>
+    public new async Task<SharedArea?> FindByIdAsync(int id)
+    {
+        return await Context.Set<SharedArea>()
+            .FirstOrDefaultAsync(sharedArea => sharedArea.Id == id);
+    }
+
+    /// <Inheritdoc/>
+    public new async Task<IEnumerable<SharedArea>> ListAsync()
+    {
+        return await Context.Set<SharedArea>()
+            .ToListAsync();
+    }
+
+    /// <Inheritdoc/>
+    public async Task<bool> ExistsByNameAsync(string name)
+    {
+        return await Context.Set<SharedArea>()
+            .AnyAsync(sharedArea => sharedArea.Name == name); 
+    }
+}
