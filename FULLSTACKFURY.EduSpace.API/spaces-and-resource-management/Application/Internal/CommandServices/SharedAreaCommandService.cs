e29d5c29449bc110ada6b273045da55b2052be21
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-11-01 00:14:19 -0500

    feat: create the command service to Shared Area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Application/Internal/CommandServices/SharedAreaCommandService.cs
@@ -0,0 +1,30 @@
+﻿using FULLSTACKFURY.EduSpace.API.Shared.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Application.Internal.CommandServices;
+
+/// <summary>
+/// Represents a shared area command service for Shared Area entities
+/// </summary>
+/// <param name="sharedAreaRepository">
+/// The repository for shared area entities
+/// </param>
+/// <param name="unitOfWork">
+/// The unit of work for the repository
+/// </param>
+public class SharedAreaCommandService(ISharedAreaRepository sharedAreaRepository, IUnitOfWork unitOfWork) : ISharedAreaCommandService
+{
+    /// <inheritDoc />
+    public async Task<sharedArea?> Handle(CreateSharedAreaCommand command)
+    {
+        if(await sharedAreaRepository.ExistsByNameAsync(command.Name))
+            throw new Exception("Shared area with the same name already exists.");
+        var sharedArea = new sharedArea(command);
+        await sharedAreaRepository.AddAsync(sharedArea);
+        await unitOfWork.CompleteAsync();
+        return sharedArea;
+    }
+}
