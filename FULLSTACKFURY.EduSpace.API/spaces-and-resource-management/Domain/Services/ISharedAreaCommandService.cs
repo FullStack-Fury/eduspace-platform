5d74c66ad5a2cad51351b33a23ca734a827059ce
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 23:32:33 -0500

    feat: create the interface to Command service to Shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Services/ISharedAreaCommandService.cs
@@ -0,0 +1,21 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Commands;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+/// <summary>
+/// Represents the shared area command service in the EduSpace API. 
+/// </summary>
+public interface ISharedAreaCommandService
+{
+    /// <summary>
+    /// Handles the create shared area command in the EduSpace API.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="CreateSharedAreaCommand"/> command to handle.
+    /// </param>
+    /// <returns>
+    /// The created <see cref="sharedArea"/> entity.
+    /// </returns>
+    Task<sharedArea?> Handle(CreateSharedAreaCommand command);
+}
