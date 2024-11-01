bf09ce949c359e114e16952d4d84744243c21901
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-10-31 23:15:22 -0500

    feat: create the query to get the shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Queries/SharedArea/GetAllSharedAreasQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+
+/// <summary>
+/// Query to get all shared areas
+/// </summary>
+public record GetAllSharedAreasQuery;
---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Domain/Model/Queries/SharedArea/GetSharedAreaByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+
+/// <summary>
+/// Query to get a shared area by id. 
+/// </summary>
+/// <param name="SharedAreaId">
+/// The id of the shared area to get.
+/// </param>
+public record GetSharedAreaByIdQuery(int SharedAreaId);
