b85ce5ff611454b561e38c19c4fcb95b090ec8d6
Andrés Torres <andresalbertotorresgarcia@gmail.com>
2024-11-01 00:14:36 -0500

    feat: Create the query service to shared area

---
+++ FULLSTACKFURY.EduSpace.API/spaces-and-resource-management/Application/Internal/QueryServices/SharedAreaQueryService.cs
@@ -0,0 +1,27 @@
+﻿using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Aggregates;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Model.Queries;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Repositories;
+using FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Domain.Services;
+
+namespace FULLSTACKFURY.EduSpace.API.spaces_and_resource_management.Application.Internal.QueryServices;
+
+/// <summary>
+/// Classroom query service
+/// </summary>
+/// <param name="classroomRepository">
+/// The classroom repository
+/// </param>
+public class SharedAreaQueryService(ISharedAreaRepository sharedAreaRepository) : ISharedAreaQueryService
+{
+    /// <Inheritdoc/>
+    public async Task<sharedArea?> Handle(GetSharedAreaByIdQuery query)
+    {
+        return await sharedAreaRepository.FindByIdAsync(query.SharedAreaId);
+    }
+    
+    /// <Inheritdoc/>
+    public async Task<IEnumerable<sharedArea>> Handle(GetAllSharedAreasQuery query)
+    {
+        return await sharedAreaRepository.ListAsync();
+    }
+}
