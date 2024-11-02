be8c306d71a9530cd50a4a8f228f6d30008c8908
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-11-01 22:18:24 -0500

    feat(profiles): all domain layer added to profiles

--- FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/Aggregates/TeacherProfile.cs
+++ FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/Aggregates/TeacherProfile.cs
@@ -21,4 +21,6 @@ public class TeacherProfile : Profile
     {
         AdministratorId = command.AdministratorId;
     }
+
+    public TeacherProfile() { }
 }
--- FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/ValueObjects/ProfilePrivateInformation.cs
+++ FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/ValueObjects/ProfilePrivateInformation.cs
@@ -7,6 +7,9 @@ public record ProfilePrivateInformation
     public string Address { get; init; }
     public string Phone { get; init; }
 
+    public ProfilePrivateInformation() : this(string.Empty, string.Empty
+        , string.Empty, string.Empty) { }
+
     public ProfilePrivateInformation(string email, string dni
         , string address, string phone)
     {
