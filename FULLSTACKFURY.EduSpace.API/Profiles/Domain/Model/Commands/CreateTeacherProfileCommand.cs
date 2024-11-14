a088cb42b49f40f80633a55ed70ee9b45dccb1c2
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-11-13 20:56:08 -0500

    refactor(profiles): command and resources refactored

--- FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/Commands/CreateTeacherProfileCommand.cs
+++ FULLSTACKFURY.EduSpace.API/Profiles/Domain/Model/Commands/CreateTeacherProfileCommand.cs
@@ -2,4 +2,4 @@ namespace FULLSTACKFURY.EduSpace.API.Profiles.Domain.Model.Commands;
 
 public record CreateTeacherProfileCommand(string FirstName, string LastName
     , string Email, string Dni, string Address, string Phone, int AdministratorId
-    , string Username, string Password, string Role);
+    , string Username, string Password);
