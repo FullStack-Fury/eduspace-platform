404fdde7ac3f5da996ce9500a321268c96b81f3b
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-11-13 20:49:13 -0500

    fix(iam): domain layer updated

--- FULLSTACKFURY.EduSpace.API/IAM/Domain/Model/Aggregates/Account.cs
+++ FULLSTACKFURY.EduSpace.API/IAM/Domain/Model/Aggregates/Account.cs
@@ -1,21 +1,36 @@
+using System.Text.Json.Serialization;
 using FULLSTACKFURY.EduSpace.API.IAM.Domain.Model.ValueObjects;
 
 namespace FULLSTACKFURY.EduSpace.API.IAM.Domain.Model.Aggregates;
 
 public class Account
 {
-    //:TODO add hashing in the future
+
     public int Id { get; }
     public string Username { get; private set; }
-    public string Password { get; private set; }
+    [JsonIgnore] public string PasswordHash { get; private set; }
     public ERoles Role { get; private set; }
-    
-    public Account(string username, string password, string role)
+
+    public Account UpdateUsername(string username)
     {
         Username = username;
-        Password = password;
-        Role = Enum.Parse<ERoles>(role);
+        return this;
     }
 
-    public Account() : this("", "", null) { }
+    public Account UpdatePasswordHash(string passwordHash)
+    {
+        PasswordHash = passwordHash;
+        return this;
+    }
+    
+    public Account(string username, string passwordHash, string role)
+    {
+        Username = username;
+        PasswordHash = passwordHash;
+        Role = Enum.Parse<ERoles>(role);
+    }
+    
+    public string GetRole() => Role.ToString();
+
+    public Account() { }
 }
