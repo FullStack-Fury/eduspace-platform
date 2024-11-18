445dc03876d76202b5e0fb0b5903bf4a857bfe7d
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-11-18 18:13:15 -0500

    feat: deployed!

--- FULLSTACKFURY.EduSpace.API/Program.cs
+++ FULLSTACKFURY.EduSpace.API/Program.cs
@@ -229,11 +229,9 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 // app.UseRouting();
