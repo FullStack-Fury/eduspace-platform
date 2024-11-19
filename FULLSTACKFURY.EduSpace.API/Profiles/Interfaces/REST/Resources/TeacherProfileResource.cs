337b96bf4000029bb33e339d2d6b3ca69ab734c2
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-11-19 10:45:14 -0500

    refactor(profiles): response with all data

--- FULLSTACKFURY.EduSpace.API/Profiles/Interfaces/REST/Resources/TeacherProfileResource.cs
+++ FULLSTACKFURY.EduSpace.API/Profiles/Interfaces/REST/Resources/TeacherProfileResource.cs
@@ -1,3 +1,4 @@
 namespace FULLSTACKFURY.EduSpace.API.Profiles.Interfaces.REST.Resources;
 
-public record TeacherProfileResource(int Id, string ProfileFullName);
+public record TeacherProfileResource(int Id, string FirstName, string LastName, string Email, string Dni
+    , string Address, string Phone);
